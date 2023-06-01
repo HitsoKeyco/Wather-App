2ddf62d39d814d007f3c04a5fa4100c195c6f555
Sergio Olivo <sergio_oliver009@hotmail.com>
2023-05-31 23:15:50 -0500

    app

--- components/WeatherCard.jsx
+++ components/WeatherCard.jsx
@@ -1,33 +1,40 @@
 import React, { useState } from 'react'
 
-const WeatherCard = ({ weather, temp }) => {
+
+const WeatherCard = ({ weather, temp, handleSubmit }) => {
 
     const [iscelcius, setIscelcius] = useState(true)
 
-const handleChangeTemp = () => {
-    setIscelcius(!iscelcius)
-}
-    
+
+    const handleChangeTemp = () => {
+        setIscelcius(!iscelcius)
+    }
+
+
+
+
+
+
     return (
         <article className='App'>
             <header className='title'>
-                <h1 className='title_app'>Weather App</h1>                
+                <h1 className='title_app'>Weather App</h1>
             </header>
             <section className='temp'>
                 <div className="left">
-                <h3>{weather?.weather[0].description}</h3>
+                    <h3>{weather?.weather[0].description}</h3>
                     <img className='img_300px' src={weather && `https://openweathermap.org/img/wn/${weather?.weather[0].icon}@4x.png`} alt="img" />
                 </div>
                 <div className="right">
-                    
+
                     <ul>
                         <li><span className='title_description'>Wind Speed: </span><span>{weather?.wind.speed} m/s</span></li>
                         <li><span className='title_description'>Clouds: </span><span>{weather?.clouds.all} %</span></li>
                         <li><span className='title_description'>Pressure: </span><span>{weather?.main.pressure} hPa</span></li>
                     </ul>
                     <div className="info_temp">
-                    <h4 className='temp_show'>{iscelcius ? `${temp?.celsius} °C` : `${temp?.farenheit} °F`}</h4>
-                    <button className='button_change_temp' onClick={handleChangeTemp}>'Change to'{iscelcius ? '°F' : '°C'}</button>
+                        <h4 className='temp_show'>{iscelcius ? `${temp?.celsius} °C` : `${temp?.farenheit} °F`}</h4>
+                        <button className='button_change_temp' onClick={handleChangeTemp}>'Change to'{iscelcius ? '°F' : '°C'}</button>
                     </div>
                 </div>
             </section>
@@ -35,16 +42,17 @@ const handleChangeTemp = () => {
             <section className='name'>
                 <h2 className='nameh3'>{weather?.name}, {weather?.sys.country}</h2>
             </section>
-            <section className='search'>
-                <input type="text" />
-                <button className='button_search'>Search</button>
-            </section>
+
+            <form action="formSearch" onSubmit={handleSubmit}>
+                <input type="text" id="inputValue" placeholder="    ...algun lugar?" />
+                <button className='button_search' type="submit">Search</button>
+            </form>
 
             <section className='img_700px'>
-            <img  src={weather && `https://openweathermap.org/img/wn/${weather?.weather[0].icon}@4x.png`} alt="img" />
+                <img src={weather && `https://openweathermap.org/img/wn/${weather?.weather[0].icon}@4x.png`} alt="img" />
             </section>
-            
-            
+
+
 
         </article>
     )
--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
   const [coords, setCoords] = useState()
   const [weather, setWeather] = useState()
   const [temp, setTemp] = useState()
+  const [search, setSearch] = useState()
 
   useEffect(() => {
     const success = pos => {
@@ -23,39 +24,62 @@ function App() {
     navigator.geolocation.getCurrentPosition(success);
   }, []);
 
+  {
+    search ?
+      useEffect(() => {
+        const url2 = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${getApiKey()}`
+        axios.get(url2)
+          .then(res => {
+            setWeather(res.data)
+            const objTemp = {
+              celsius: +(res.data.main.temp - 273.15).toFixed(0),
+              farenheit: +((res.data.main.temp - 273.15) * 9 / 5 + 32).toFixed(0)
+            }
+            setTemp(objTemp)
+          })
+          .catch(err => console.log(err));
+      }, [search])
+      :
+      useEffect(() => {
+        if (coords) {
+          const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${getApiKey()}`
+          axios.get(url)
+            .then(res => {
+              setWeather(res.data)
+              const objTemp = {
+                celsius: +(res.data.main.temp - 273.15).toFixed(0),
+                farenheit: +((res.data.main.temp - 273.15) * 9 / 5 + 32).toFixed(0)
+              }
+              setTemp(objTemp)
+            })
+            .catch(err => console.log(err));
+        }
+      }, [coords]);
+  }
 
-  useEffect(() => {
-    if (coords) {
-      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${getApiKey()}`
-      axios.get(url)
-        .then(res => {
-          setWeather(res.data)
-          const objTemp = {
-            celsius: +(res.data.main.temp - 273.15).toFixed(0),
-            farenheit: +((res.data.main.temp - 273.15) * 9/5 + 32).toFixed(0)
-          }
 
-          setTemp(objTemp)
-
-
-        })
-        .catch(err => console.log(err));
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const inputValue = e.target.inputValue.value.trim();
+    if (inputValue) {
+      setSearch(inputValue);
     }
-  }, [coords]);
+    e.target.inputValue.value = '';
+  };
 
-  console.log(weather);
+  console.log(search);
 
   return (
     <>
-    {
-      weather
-      ? <WeatherCard 
-          weather={weather} 
-          temp={temp}
-      />
-      : <Loading />
-    }
-  </>
+      {
+        weather
+          ? <WeatherCard
+            weather={weather}
+            temp={temp} handleSubmit={handleSubmit}
+          />
+          : <Loading />
+      }
+    </>
   )
 }
 
